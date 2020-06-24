08ee16ea2e9e0574f6f23fa78fcab09bf1246a50
leruse4ka <60311701+leruse4ka@users.noreply.github.com>
2020-06-24 11:10:03 +0500

    Исправления

--- js/dialog.js
+++ js/dialog.js
@@ -24,6 +24,8 @@
 
   var closePopup = function () {
     setup.classList.add('hidden');
+    setup.style.top = 80 + 'px';
+    setup.style.left = 50 + '%';
 
     document.removeEventListener('keydown', onPopupEscPress);
   };
